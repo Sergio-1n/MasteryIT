051122dafcaf4cf15672c187c23e06746e232779
Sergio-1n <skuznetcov150@gmail.com>
2025-05-08 17:34:58 +0200

    corrected div

--- app/book/layout.tsx
+++ app/book/layout.tsx
@@ -1,15 +1,7 @@
-// app/book/layout.tsx
 export default function BookLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return <>{children}</>;
 }
